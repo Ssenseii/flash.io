f6216f1fd412b067cc91ae44f0c38d677b7619ca
Simon <89594899+Ssenseii@users.noreply.github.com>
2024-11-09 23:49:25 +0100

    added 404 not found page

--- src/pages/misc/NotFound.vue
+++ src/pages/misc/NotFound.vue
@@ -1,5 +1,9 @@
 <template>
-    <h1>404 - Not Found</h1>
+    <div class="notfound_page">
+        <h1>404 - Page Not Found</h1>
+        <p>It seems the page you're looking for does not exist</p>
+        <router-link to="/">Go Back To Home Screen</router-link>
+    </div>
 </template>
 
 <script setup>
